b9a2c014280391ba4cc4f35b79c9798e4ec55b0f
Marvin Berrio <marvinccp@gmail.com>
2023-07-14 11:36:46 +0200

    Create a backGround with particles

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
+'use client'
 import Image from "next/image";
 import styles from "../styles/page.module.css";
 import Link from "next/link";
 import Main from "@/components/Main";
+import BackGround from "@/components/BackGround";
 
 export default function Home() {
   return (
     <section className={styles.container}>
+      <BackGround />
       <Main />
     </section>
   );
