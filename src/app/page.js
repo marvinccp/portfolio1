3e7e68cf63c424886d779c88055db4d551326ffb
Marvin Berrio <marvinccp@gmail.com>
2023-07-20 01:05:45 +0200

    Resolve error page/styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,10 @@
-'use client'
-import Image from "next/image";
-import styles from "../styles/page.module.css";
-import Link from "next/link";
+"use client";
 import Main from "@/components/main/Main";
-import BackGround from "@/components/BackGround";
 
 export default function Home() {
   return (
     <>
-      
       <Main />
-      
     </>
   );
 }
