525df5244b32b7e6dd27ce85c926b3c05d7bfadb
Marvin Berrio <marvinccp@gmail.com>
2023-07-19 02:37:20 +0200

    update modal styles

--- src/app/utils/dataProjects.js
+++ src/app/utils/dataProjects.js
@@ -1,16 +1,21 @@
 export const dataProjects = [
   {
-    id: '1',
+    id: "1",
     title: "Master Question",
-    description: "lorem ipsum nfgdfg ethewtru wegasgesh st eth  eth ",
+    description: "MQ es el prototipo de un juego sencillo de preguntas y respuestas tipo test. En proyecto he consgtruido el sistema de creación y modificación de preguntas, ademas de la conexión a su bse de datos",
     link: "https://google.com",
     github: "https://github.com",
     image: "/projects_images/master_question.png",
-    tags: ["React", "Javascript", "CSS"],
+    page_image: "/projects_images/master_question.png",
+    tec:[
+      {tec:'Javascript',image:'/icons/js.png'},
+      {tec:'CSS',image:'/icons/css.png'},
+    ],
+    tags: ["React", "Javascript", "CSS", 'Game', 'Node', 'Web Developer'],
     youtube: "https://www.youtube.com",
   },
   {
-    id: '2',
+    id: "2",
     title: "Project 1",
     description: "lorem ipsum nfgdfg ethewtru wegasgesh st eth  eth ",
     link: "https://google.com",
@@ -20,7 +25,7 @@ export const dataProjects = [
     youtube: "https://www.youtube.com",
   },
   {
-    id: '3',
+    id: "3",
     title: "Project 1",
     description: "lorem ipsum nfgdfg ethewtru wegasgesh st eth  eth ",
     link: "https://google.com",
@@ -30,7 +35,7 @@ export const dataProjects = [
     youtube: "https://www.youtube.com",
   },
   {
-    id: '4',
+    id: "4",
     title: "Project 1",
     description: "lorem ipsum nfgdfg ethewtru wegasgesh st eth  eth ",
     link: "https://google.com",
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,19 +1,77 @@
+"use client";
 import React from "react";
 import ImageLayout from "./Image";
 import styles from "../styles/content.module.css";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
 const Content = ({ data }) => {
-  const { id, image, title } = data;
+  const { id, image, title, description, tec, tags } = data;
+  const router = useRouter();
+
   return (
     <section className={styles.content}>
-      <header className={styles.container}>
+      <div className={styles.back_container}>
+        <span onClick={() => router.back()}></span>
+        <h5 onClick={() => router.back()}>Back to projects</h5>
+      </div>
+      <header>
         <ImageLayout src={image} width={150} height={63} alt={title} />
+        <div className={styles.text_container}>
+          <h3>{title}</h3>
+          <p>{description}</p>
+        </div>
       </header>
       <main>
-        <h3>{title}</h3>
-        <h1>main</h1>
+        <h3>Tecnologías</h3>
+        <section className={styles.tec_container}>
+          {tec.map(({ tec, image }) => (
+            <ImageLayout
+              key={tec}
+              src={image}
+              width={30}
+              height={30}
+              alt={tec}
+            />
+          ))}
+        </section>
+        <div className={styles.links}>
+          <h3>Links</h3>
+          <section className={styles.demo_links}>
+            <Link href={"/"}>
+              <ImageLayout
+                src={"/icons/github.svg"}
+                alt={"github_logo"}
+                width={50}
+                height={50}
+                className={styles.icon}
+              />
+            </Link>
+            <Link href={"/"}>
+              <ImageLayout
+                src={"/icons/youtube.svg"}
+                alt={"youtube_logo"}
+                width={50}
+                height={50}
+                className={styles.icon}
+              />
+            </Link>
+          </section>
+        </div>
       </main>
       <footer>
-        <h1>footer</h1>
+        <h3>Tags</h3>
+        <h5>{tags.join("    ,    ")}</h5>
+        <button>
+          Open Project
+          <ImageLayout
+            src={"/icons/go.png"}
+            alt={"youtube_logo"}
+            width={25}
+            height={25}
+            className={styles.icon}
+          />
+        </button>
       </footer>
     </section>
   );
