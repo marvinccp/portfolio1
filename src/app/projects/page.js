e257bd5e998c20b3e0ad3cb75a64cefbf16edf1b
Marvin Berrio <marvinccp@gmail.com>
2023-07-21 00:33:23 +0200

    Update mini projects

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -3,12 +3,12 @@ import styles from "./projects.module.css";
 import ImageLayout from "../../components/Image";
 import Link from "next/link";
 import { dataProjects } from "../utils/dataProjects";
+import { miniProjects } from "../utils/miniProjects";
+
 console.log(dataProjects);
 const ListProjects = () => {
   return (
     <section className={styles.container}>
-
-
       <div className={styles.title}>
         <div className={styles.menu_inline}>
           <div className={styles.line}></div>
@@ -24,7 +24,6 @@ const ListProjects = () => {
         </div>
         <h1>Projects</h1>
       </div>
-
 
       <main className={styles.main}>
         {dataProjects.map(({ id, image, title }) => (
@@ -42,9 +41,22 @@ const ListProjects = () => {
         ))}
       </main>
 
-
-
-      
+      <h2>mini Projects</h2>
+      <main className={styles.main}>
+        {miniProjects.map(({ id, image, title, link }) => (
+          <Link key={id} href={link} target="_blank" rel="noopener noreferrer">
+            <div className={styles.card}>
+              <ImageLayout
+                src={image}
+                alt={title}
+                width={200}
+                height={93}
+                className={styles.image}
+              />
+            </div>
+          </Link>
+        ))}
+      </main>
     </section>
   );
 };
---
+++ src/app/utils/miniProjects.js
@@ -0,0 +1,8 @@
+export const miniProjects = [
+  {
+    id: "1",
+    title: "My Travel Journal",
+    link: "https://travel-journal-react-js.vercel.app/",
+    image: "/projects_Images/travel.png",
+  },
+];
