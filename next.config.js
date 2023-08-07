10d88481335eaf783d277209e32dce470371b19f
Marvin Berrio <marvinccp@gmail.com>
2023-08-07 14:18:27 +0200

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
