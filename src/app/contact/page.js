1dffc481fd5f707fc78157926a686bd858d8dce0
Marvin Berrio <marvinccp@gmail.com>
2023-11-02 23:42:52 +0100

    Update contact page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -19,57 +19,61 @@ const Contact = () => {
             <p>about</p>
           </Link>
         </div>
+
         <h1>Contact</h1>
-        <p className={styles.subtitle_contact}>
+        <p className={styles.subtitle_contact_a}>
           Hola, dejame saber si puedo ayudarte en algo
         </p>
-        <h3>Email:</h3>
-        <Link href={"mailto:contacto@marvinberrio.com"}>
-          <p className={styles.subtitle_contact}>contacto@marvinberrio.com</p>
-        </Link>
-        <section className={styles.rrss_contact}>
-          <Link
-            target="_blank"
-            rel="noopener noreferrer"
-            href={"https://wa.me/34641732384"}
-          >
-            <ImageLayout
-              src={"/icons/whatsapp.svg"}
-              alt={"whatsapp_logo"}
-              width={42}
-              height={42}
-              className={styles.icon_contact}
-            />
+        <main className={styles.main}>
+          <Link href={"mailto:contacto@marvinberrio.com"}>
+            <h3>Email:</h3>
+            <p className={styles.subtitle_contact}>contacto@marvinberrio.com</p>
           </Link>
 
-          <Link
-            target="_blank"
-            rel="noopener noreferrer"
-            href={"https://www.linkedin.com/in/marvinberrio/"}
-          >
-            <ImageLayout
-              src={"/icons/linkedin.svg"}
-              alt={"linkedin_logo"}
-              width={42}
-              height={42}
-              className={styles.icon_contact}
-            />
-          </Link>
+          <section className={styles.rrss_contact}>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              href={"https://wa.me/34641732384"}
+            >
+              <ImageLayout
+                src={"/icons/whatsapp.svg"}
+                alt={"whatsapp_logo"}
+                width={42}
+                height={42}
+                className={styles.icon_contact}
+              />
+            </Link>
 
-          <Link
-            target="_blank"
-            rel="noopener noreferrer"
-            href={"https://www.youtube.com/@tevstudio"}
-          >
-            <ImageLayout
-              src={"/icons/youtube.svg"}
-              alt={"youtube_logo"}
-              width={41}
-              height={41}
-              className={styles.icon_contact}
-            />
-          </Link>
-        </section>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              href={"https://www.linkedin.com/in/marvinberrio/"}
+            >
+              <ImageLayout
+                src={"/icons/linkedin.svg"}
+                alt={"linkedin_logo"}
+                width={42}
+                height={42}
+                className={styles.icon_contact}
+              />
+            </Link>
+
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              href={"https://www.youtube.com/@tevstudio"}
+            >
+              <ImageLayout
+                src={"/icons/youtube.svg"}
+                alt={"youtube_logo"}
+                width={41}
+                height={41}
+                className={styles.icon_contact}
+              />
+            </Link>
+          </section>
+        </main>
       </div>
     </section>
   );
