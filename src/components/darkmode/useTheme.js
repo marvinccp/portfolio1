7a886abaa273d7cfca20ee6783cf017bc627244f
Marvin Berrio <marvinccp@gmail.com>
2023-11-23 23:49:46 +0100

    Fix localStorage error

--- src/components/darkmode/useTheme.js
+++ src/components/darkmode/useTheme.js
@@ -1,9 +1,8 @@
 import { useEffect, useState } from "react";
 
 export const useTheme = () => {
-  
-  const initialState = window !== undefined ? localStorage.getItem("theme"): ''
-      
+  const initialState =
+    typeof window !== "undefined" ? ocalStorage.getItem("theme") : "";
 
   const [theme, setTheme] = useState(initialState);
 
